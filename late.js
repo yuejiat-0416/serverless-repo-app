b6b825f57eaf87a1bf818de50f1a7e01aab5134c
Yuejia Tong <101756281+yuejiat-0416@users.noreply.github.com>
2022-04-27 19:49:55 +0800

    Update late.js

--- late.js
+++ late.js
@@ -1,9 +1,10 @@
 // start coding your function here!
 function running_late(date) {
-  if (date.getHours() === 8){
+  d = new Date(date);
+  if (d.getHours() === 8){
     return "It is still early!";
   }
-  else if(date.getHours() === 23 && date.getMinutes() == 30){
+  else if(d.getHours() === 23 && d.getMinutes() == 30){
     return "It is late!";
   }
 }
